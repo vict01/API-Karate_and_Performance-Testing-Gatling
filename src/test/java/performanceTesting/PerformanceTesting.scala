828186ef3b42611135360ab8fa176e9814d3bb62
VictorCaminero <v.caminero@gbh.com>
2020-12-02 17:11:59 -0400

    After updating the duration without needing <import scala.concurrent.duration._> because is implicit as seconds in the setUp without indicating with the word "seconds".

--- src/test/java/performanceTesting/PerformanceTesting.scala
+++ src/test/java/performanceTesting/PerformanceTesting.scala
@@ -1,17 +1,15 @@
 package performanceTesting
 
+import io.gatling.core.scenario.Simulation
 import com.intuit.karate.gatling.PreDef._
 import io.gatling.core.Predef._
-import io.gatling.core.scenario.Simulation
-import scala.concurrent.duration._
-
 
 class PerformanceTesting extends Simulation {
 
     val userAuthentication = scenario(scenarioName="Authentication test").exec(karateFeature( name="classpath:feature/userAuth.feature"))
 
     setUp(
-            userAuthentication.inject(rampUsers(users=10).during(10 seconds))
+            userAuthentication.inject(rampUsers(users=10).during(10))
     )
 
 }
